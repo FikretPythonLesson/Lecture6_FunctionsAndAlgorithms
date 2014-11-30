b94a247d9249eb682f4d9ce2a2e36ff1d1898e03
FikretPythonLesson <FikretPythonLesson@users.noreply.github.com>
2014-11-30 17:12:08 +0200

    Update ticket.py
    A bus company charges 1 TL per ticket if you buy fewer than five tickets; otherwise they cost 75 KURUS each. Write a function  that will ask for the number of tickets that you require, and tell you the total cost.

--- ticket.py
+++ ticket.py
@@ -1,9 +1,9 @@
-biletSayisi = input("adet")
+biletSayisi = input("number of tickets")
 while biletSayisi > 0:
     if biletSayisi < 5:
         toplamOdeme = biletSayisi * 3
     elif biletSayisi >= 5:
         toplamOdeme = biletSayisi * 1
     print toplamOdeme
-    biletSayisi = input("adet")
+    biletSayisi = input("number of tickets")
     
